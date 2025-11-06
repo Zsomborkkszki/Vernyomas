dcfaf34ca050e73d4c700221c6f06efc4e3cc505
Zsombor <szilagyizs@kkszki.hu>
2025-11-06 13:25:45 +0100

    teszt

--- Vernyomas/Program.cs
+++ Vernyomas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Vernyomas
 {
@@ -10,6 +11,7 @@ namespace Vernyomas
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
